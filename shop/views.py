cdb21b53a243cd1a1fcdc9f885fd0c226176ea87
Chris West <chris@minrivertea.com>
2011-05-06 20:55:33 +0800

    templates changes

--- shop/views.py
+++ shop/views.py
@@ -76,7 +76,16 @@ def index(request):
     
 # the product listing page
 def teas(request):
-            
+    try:
+        added = request.session['ADDED']
+    except:
+        added = None
+        
+    if added:
+        thing = get_object_or_404(BasketItem, id=request.session['ADDED'])
+        message = "1 x %s%s added to your basket!" % (thing.item.weight, thing.item.weight_unit)
+        request.session['ADDED'] = None
+        
     products = Product.objects.filter(category="TEA", is_active=True)
     prices = UniqueProduct.objects.filter(is_active=True)
     products_and_prices = []
