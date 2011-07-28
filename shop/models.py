72f923975cca5515a56cccd7167216e5f53c1be7
Chris West <chris@minrivertea.com>
2011-07-28 15:33:15 +0800

    coming soon changes

--- shop/models.py
+++ shop/models.py
@@ -147,6 +147,7 @@ class Product(models.Model):
     category = models.CharField(max_length=3, choices=PRODUCT_CATEGORY)
     is_featured = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    coming_soon = models.BooleanField(default=False)
         
     def __unicode__(self):
         return self.name
