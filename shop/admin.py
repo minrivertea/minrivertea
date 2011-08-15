823edaa9be99ac309ff26109870b9ec965bb0cdf
Chris West <chris@minrivertea.com>
2011-08-15 14:23:37 +0800

    changes

--- shop/admin.py
+++ shop/admin.py
@@ -5,7 +5,7 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
    
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('invoice_id', 'is_paid', 'notes')
+    list_display = ('invoice_id', 'is_paid', 'owner', 'notes')
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('product', 'email', 'is_published')
