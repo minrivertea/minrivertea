2b22bc618f448463fed466d1ebe8d591a2816a17
Chris <chris@fry-it.com>
2012-02-12 14:13:44 +0800

    change to sitemap

--- shop/models.py
+++ shop/models.py
@@ -415,17 +415,7 @@ class Page(models.Model):
         return items
     
     def get_absolute_url(self):
-        if self.parent:
-            if self.parent.parent:
-                if self.parent.parent.parent:
-                    url = "/%s/%s/%s/%s/" % (self.parent.parent.parent.slug, self.parent.parent.slug, self.parent.slug, self.slug)
-                else:
-                    url = "/%s/%s/%s/" % (self.parent.parent.slug, self.parent.slug, self.slug)
-            else:
-                url = "/%s/%s/" % (self.parent.slug, self.slug)
-        else:
-            url = "/%s/" % (self.slug)
-            
+        url = "/%s/" % self.slug  
         return url
 
     def get_products_mentioned(self):
