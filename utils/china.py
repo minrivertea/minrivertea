4a94acc370d97896e7db83af996c5c579ae7cec9
Chris West <chris@minrivertea.com>
2012-06-07 15:08:21 +0800

    changes

--- utils/china.py
+++ utils/china.py
@@ -22,7 +22,7 @@ class ChinaMiddleware:
             except:
                 # detect if they're in China or not!
                 countrycode = GetCountry(request)['countryCode']
-                if countrycode == "-":
+                if countrycode == "CN":
                     request.session['region'] = 'china' 
                     settings.TEMPLATE_DIRS = settings.CHINA_TEMPLATES_DIR
             
