26e8f9d8f680e4f7b62a5b865160532e3d85e586
Chris West <chris@minrivertea.com>
2012-10-25 16:54:29 +0800

    fixed error on pages subnav

--- settings.py
+++ settings.py
@@ -24,6 +24,8 @@ DATABASE_HOST = ''             # Set to empty string for localhost. Not used wit
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 
+
+
 TIME_ZONE = 'Europe/London'
 SITE_ID = 1
 SITE_URL = "http://www.minrivertea.com"
