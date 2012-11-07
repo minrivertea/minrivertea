80cda3ab4f501c1a7bcb894a1533ff5eb2e83140
Chris West <chris@minrivertea.com>
2012-11-07 17:27:26 +0800

    transfer orders added

--- shop/admin.py
+++ shop/admin.py
@@ -26,7 +26,7 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
    
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('invoice_id', 'is_paid', 'owner', 'notes')
+    list_display = ('invoice_id', 'is_paid', 'owner', 'notes',)
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('product', 'email', 'is_published')
