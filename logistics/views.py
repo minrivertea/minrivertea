8e55f24858169f6112132b871aa5cb4b92208948
minrivertea <chris@minrivertea.com>
2012-12-04 21:59:19 +0800

    translation updates, logistics changes

--- logistics/views.py
+++ logistics/views.py
@@ -11,37 +11,36 @@ from logistics.forms import UpdateCustomerPackageForm, AddStocksForm
 from shop.models import Currency, UniqueProduct, Order
 
 
-
-
 def _create_customer_package(order):
+    """
+    Given an order that has been paid for, create
+    a customer package and assign the appropriate 
+    stocks to that package. If the items are out 
+    of stock, then this function should create new 
+    stocks and a seperate Customer Package and mark 
+    it as a pre-order
+    """
     
-    # create a CustomerPackage
-    package = CustomerPackage.objects.create(
-        order=order,
-        created=datetime.datetime.now(),
-    )
-    
+    package = None
     preorder_package = None
     
-    # iterate through the items the customer has ordered
     for x in order.items.all():
-        
-        # remember they might have ordered 2+ of the same
         loop = x.quantity
         while loop >= 1:
             try:
-                
-                # if we have a WHItem available, then add it to the main package
-                wh_item = WarehouseItem.objects.filter(unique_product=x.item, sold__isnull=True, available__lte=datetime.datetime.now())[0]
+                wh_item = WarehouseItem.objects.filter(unique_product=x.item, sold__isnull=True)[0]
+                if not package:
+                    package = CustomerPackage.objects.create(
+                        order=order,
+                        created=datetime.datetime.now(),
+                    )
                 package.items.add(wh_item)
                 package.save()
                 wh_item.sold = datetime.datetime.now()
                 wh_item.reason = WarehouseItem.SOLD
                 wh_item.save()
             
-            except:
-                
-                # if there isn't a WHItem available, create a new one...
+            except:                
                 wh_item = WarehouseItem.objects.create(
                     unique_product=x.item,
                     hashkey=uuid.uuid1().hex,
@@ -49,8 +48,7 @@ def _create_customer_package(order):
                     batch='TEMP',
                 )
                 
-                # ...and then add it to a preorder package. 
-                if preorder_package == None:
+                if not preorder_package:
                     preorder_package = CustomerPackage.objects.create(
                         order=order,
                         created=datetime.datetime.now(),
