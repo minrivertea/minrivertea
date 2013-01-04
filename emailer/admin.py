8347de413447fb78a33528c9ea1882bdf470f136
minrivertea <chris@minrivertea.com>
2013-01-04 20:13:24 +0800

    fixed random unicode error

--- emailer/admin.py
+++ emailer/admin.py
@@ -2,7 +2,8 @@ from emailer.models import *
 from django.contrib import admin
 
 class SubscriberAdmin(admin.ModelAdmin):
-    list_display = ('email', 'date_unsubscribed',)
+    list_display = ('email', 'date_unsubscribed', 'language')
+    list_filter = ('language',)
 
 
 admin.site.register(Subscriber, SubscriberAdmin)    
