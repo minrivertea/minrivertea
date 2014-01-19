a736e231350f4509ceb0b3d013e25d4be8e9898b
Chris West <chris@minrivertea.com>
2014-01-19 13:54:39 +0800

    update to sites middleware

--- sites.py
+++ sites.py
@@ -12,28 +12,26 @@ class DomainTrackerMiddleware(object):
     """
     
     def process_request(self, request):
-        
-        
-        
+                
         # don't want to keep validating against images and CSS/JS files
         if re.match('^.+\.(jpg|jpeg|gif|png|ico|css|js)', request.path):
             return None
         
+        
         try:    
-            logger.error(request.META['HTTP_REFERER']) 
+            referer = request.META['HTTP_REFERER']
+            if referer == settings.GERMAN_URL:
+                _changelang(request, 'de') # CHANGE LANGUAGE 
+                if 'CURRENCY' not in request.session: 
+                    _set_currency(request, 'EUR') # CHANGE CURRENCY TO EUR 
+                                
+            if referer == settings.ITALIAN_URL:
+                _changelang(request, 'it') # CHANGE LANGUAGE
+                if 'CURRENCY' not in request.session: 
+                    _set_currency(request, 'EUR') # CHANGE CURRENCY TO EUR
+            
         except:
             pass
-                                            
-        if request.META['SERVER_NAME'] == settings.GERMAN_URL: # CHECK IF THE DOMAIN NAME IS .de
-            _changelang(request, 'de') # CHANGE LANGUAGE TO GERMAN
-            if 'CURRENCY' not in request.session: 
-                _set_currency(request, 'EUR') # CHANGE CURRENCY TO EUR IF NO PREFERENCE ALREADY SET
-        
-        
-        if request.META['SERVER_NAME'] == settings.ITALIAN_URL: # CHECK IF THE DOMAIN NAME IS .de
-            _changelang(request, 'it') # CHANGE LANGUAGE TO GERMAN
-            if 'CURRENCY' not in request.session: 
-                _set_currency(request, 'EUR') # CHANGE CURRENCY TO EUR IF NO PREFERENCE ALREADY SET
-        
+                       
         return None
     
