c7ae8ccfa82c623ce27a2b1918cb8b948a7f31e7
Chris West <chris@minrivertea.com>
2014-11-07 12:32:11 +0800

    update to blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 
 class BlogEntryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ('date_added', 'title', 'is_draft')
+    list_display = ('date_added', 'title', 'tagged_with', 'is_draft')
+    
+    def tagged_with(self,  obj):
+        """Returns a set of tags"""
+        return  obj.tags.names()
 
 
 admin.site.register(BlogEntry, BlogEntryAdmin)
